44fe34d78a02fb63d9f725c03cf789c60b1a9619
yusufoner77 <yusufoner7@gmail.com>
2024-01-20 18:50:13 -0600

    added comments and made bounds optional

--- oops.cpp
+++ oops.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 
-int prompt(int lowerBound, int upperBound, std::string input, std::string error) {
+// function asks user for an input, and assigns their number to int value
+// string "input" is message that will be displayed to user asking for value
+
+int prompt(std::string input, std::string error, int lowerBound = -500, int upperBound = 500 ) {
 	int value;
 	std::cout << input << std::endl;
 	std::cin >> value;
+	
+	// lowerBound is smallest valid integer, defaults to -500 if not specified
+	// upperBound is largest valid integer, defaults to 500 if not specified
+	// string error is message displayed to user if value is out of bounds
 	
 	while ( value < lowerBound || value > upperBound){
 		std::cout << error << std::endl;
@@ -15,7 +22,7 @@ int prompt(int lowerBound, int upperBound, std::string input, std::string error)
 }
 
 int main() {
-	int value = prompt(0, 100, "Please enter a value", "Your value is invalid");
+	int value = prompt("Please enter an integer", "Your value is invalid", 0, 100);
     std::cout << "The value chosen by the user is " << value << std::endl;
     return 0;
 }

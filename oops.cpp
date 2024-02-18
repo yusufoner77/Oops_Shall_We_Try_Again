82765c5d1e541d3bfcf33694efd452d1a2e71e3b
yusufoner77 <yusufoner7@gmail.com>
2024-02-17 19:23:11 -0600

    added Default Values and Abort the Entry for portfolio

--- oops.cpp
+++ oops.cpp
@@ -1,20 +1,33 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 // function asks user for an input, and assigns their number to int value
 // string "input" is message that will be displayed to user asking for value
 
-int prompt(std::string input, std::string error, int lowerBound = -500, int upperBound = 500 ) {
-	int value;
-	std::cout << input << std::endl;
-	std::cin >> value;
+string prompt(string intro, string error, int lowerBound = -500, int upperBound = 500 ) {
+	string value;
+	string defaultValue = "100";
+	
+	cout << intro << endl;
+	cin >> value;
+
+	if (value == "default") {
+		value = defaultValue;
+	}
+
+	if (value == "exit") {
+		exit(0);
+	}
 	
 	// lowerBound is smallest valid integer, defaults to -500 if not specified
 	// upperBound is largest valid integer, defaults to 500 if not specified
 	// string error is message displayed to user if value is out of bounds
 	
-	while ( value < lowerBound || value > upperBound){
+	while ( stoi(value) < lowerBound || stoi(value) > upperBound){
 		std::cout << error << std::endl;
-		std::cout << input << std::endl;
+		std::cout << intro << std::endl;
 		std::cin >> value;
 	}
 	
@@ -22,7 +35,9 @@ int prompt(std::string input, std::string error, int lowerBound = -500, int uppe
 }
 
 int main() {
-	int promptValue = prompt("Please enter an integer", "Your value is invalid", 0, 100);
+	string intro = "Please enter an integer. Enter \"default\" to use the default value of 100. Enter \"exit\" to leave the menu.";
+	string error = "Your value is invalid.";
+	string promptValue = prompt(intro, error, 0, 100);
     std::cout << "The value chosen by the user is " << promptValue << std::endl;
     return 0;
 }
